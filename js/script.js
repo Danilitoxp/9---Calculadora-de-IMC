dedc7376cb3350d67a3078ec811ad9f3002c9c20
Danilo Oliveira <danilooliveira.contato@hotmail.com>
2023-12-18 20:27:15 -0300

    adicionando alertas, e modularizando as funçoes utilitarias

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 import { Modal } from './modal.js'
+import { AlertError } from "./alert-error.js"
+import { IMC, notANumber } from "./utils.js"
 
 // variáveis - variables
 const form = document.querySelector('form')
@@ -12,13 +14,18 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if (showAlertError) {
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
+
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
     Modal.open()
-}
-
-function IMC(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
 }
